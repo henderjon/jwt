4a6afe858433aa09cfe82c312cd95f6f79ee00b2
henderjon <list@box370.net>
2019-05-23 12:12:26 -0500

    more cleanup

--- hmac.go
+++ hmac.go
@@ -1,15 +1,18 @@
 package jwt
 
 import (
+	"crypto"
 	"crypto/hmac"
 	"errors"
+	"hash"
 	// this lib had to be linked at compile time
 	_ "crypto/sha256"
+	_ "crypto/sha512"
 )
 
 // HMACSigner will sign a JWT using crypto.SHA256
 type HMACSigner struct {
-	name        string
+	name       string
 	signerFunc signerFunc
 }
 
@@ -36,4 +39,35 @@ func (s *HMACSigner) Verify(json, signature string) error {
 		return errors.New("invalid signature")
 	}
 	return nil
+}
+
+func makeHMACSignerFunc(f func() hash.Hash, secret []byte) signerFunc {
+	return func(json string) []byte {
+		h := hmac.New(f, secret)
+		h.Write([]byte(json))
+		return h.Sum(nil)
+	}
+}
+
+// NewHMACSigner is a factory for signers of the HMAC hash type
+func NewHMACSigner(alg SigningHash, secret []byte) *HMACSigner {
+	switch alg {
+	default:
+		panic("hash not implemented")
+	case HS256:
+		return &HMACSigner{
+			name:       "HS256",
+			signerFunc: makeHMACSignerFunc(crypto.SHA256.New, secret),
+		}
+	case HS384:
+		return &HMACSigner{
+			name:       "HS384",
+			signerFunc: makeHMACSignerFunc(crypto.SHA384.New, secret),
+		}
+	case HS512:
+		return &HMACSigner{
+			name:       "HS512",
+			signerFunc: makeHMACSignerFunc(crypto.SHA512.New, secret),
+		}
+	}
 }
--- signer.go
+++ signer.go
@@ -1,14 +1,5 @@
 package jwt
 
-import (
-	"crypto"
-	"crypto/hmac"
-	"hash"
-	// this lib had to be linked at compile time
-	_ "crypto/sha256"
-	_ "crypto/sha512"
-)
-
 // SigningHash is a custom type for specifying the algorithm used
 type SigningHash uint
 
@@ -37,53 +28,3 @@ type Signer interface {
 
 // signerFunc is a signing function
 type signerFunc func(json string) []byte
-
-func makeHMACSignerFunc(f func() hash.Hash, secret []byte) signerFunc {
-	return func(json string) []byte {
-		h := hmac.New(f, secret)
-		h.Write([]byte(json))
-		return h.Sum(nil)
-	}
-}
-
-// NewHMACSigner is a factory for signers of the HMAC hash type
-func NewHMACSigner(alg SigningHash, secret []byte) *HMACSigner {
-	switch alg {
-	default:
-		panic("hash not implemented")
-	case HS256:
-		return &HMACSigner{
-			name:       "HS256",
-			signerFunc: makeHMACSignerFunc(crypto.SHA256.New, secret),
-		}
-	case HS384:
-		return &HMACSigner{
-			name:       "HS384",
-			signerFunc: makeHMACSignerFunc(crypto.SHA384.New, secret),
-		}
-	case HS512:
-		return &HMACSigner{
-			name:       "HS512",
-			signerFunc: makeHMACSignerFunc(crypto.SHA512.New, secret),
-		}
-	}
-}
-
-// NewRSASigner is a factory for signers of the RSA hash type
-// func NewRSASigner(alg SigningHash, public *rsa.PublicKey, private *rsa.PrivateKey) {
-// panic("hash not implemented")
-// RS256
-// RS384
-// RS512
-// PS256
-// PS384
-// PS512
-// }
-
-// NewECDSASigner is a factory for signers of the ECDSA hash type
-// func NewECDSASigner(alg SigningHash) {
-// panic("hash not implemented")
-// ES256
-// ES384
-// ES512
-// }
